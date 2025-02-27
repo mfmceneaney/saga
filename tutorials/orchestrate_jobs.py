4d4486bf902bac4b32d7f194c9fb9fc081a3b36e
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-02-27 14:51:39 -0500

    feat: Added job orchestration python tutorial.

---
+++ tutorials/orchestrate_jobs.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append('../py')
+
+from saga.orchestrate import create_jobs, submit_jobs
+
+# Create job submission structure
+methods = {"method":["HB","LF"]}
+fitvars = {"asymfitvars":[["costheta1"],["costheta2"]]}
+seeds   = {"inject_seed":[2**i for i in range(4)]}
+
+# Results file paths and config
+base_dir    = "results/"
+submit_path = base_dir+"submit.sh"
+yaml_path   = base_dir+"args.yaml"
+out_path    = base_dir+"jobs.txt"
+divisions = dict(
+    methods,
+    **fitvars,
+    **seeds
+)
+create_jobs(divisions,base_dir,submit_path,yaml_path)
+submit_jobs(divisions,base_dir,submit_path,out_path,dry_run=True,generate_dummy_data=True)
