d74e17e70661cd242f62595ffb3540ea3a7772c8
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-06-19 14:17:32 -0400

    fix: Clean up `orchestrate` module formatting with black.

--- py/saga/orchestrate.py
+++ py/saga/orchestrate.py
@@ -12,13 +12,10 @@ import shutil
 import yaml
 from .aggregate import get_config_list, get_config_str
 
+
 def create_jobs(
-        configs,
-        base_dir="./",
-        submit_path="submit.sh",
-        yaml_path="args.yaml",
-        aliases=None
-    ):
+    configs, base_dir="./", submit_path="submit.sh", yaml_path="args.yaml", aliases=None
+):
     """
     Parameters
     ----------
@@ -49,42 +46,43 @@ def create_jobs(
     for data_list_i in data_list:
 
         # Make job directory name and directory
-        job_dir = os.path.join(base_dir, get_config_str(data_list_i,aliases=aliases))
+        job_dir = os.path.join(base_dir, get_config_str(data_list_i, aliases=aliases))
         data_list_i["outdir"] = os.path.abspath(job_dir)
         try:
             os.mkdir(job_dir)
         except FileExistsError:
-            print("WARNING: ",job_dir,"already exists.")
+            print("WARNING: ", job_dir, "already exists.")
 
         # Copy files into job directory
-        submit_path_i = os.path.join(job_dir,os.path.basename(submit_path))
-        yaml_path_i   = os.path.join(job_dir,os.path.basename(yaml_path))
-        shutil.copyfile(submit_path,submit_path_i)
-        shutil.copyfile(yaml_path,yaml_path_i)
+        submit_path_i = os.path.join(job_dir, os.path.basename(submit_path))
+        yaml_path_i = os.path.join(job_dir, os.path.basename(yaml_path))
+        shutil.copyfile(submit_path, submit_path_i)
+        shutil.copyfile(yaml_path, yaml_path_i)
 
         # Replace key values in yaml file from list and insert if non-existent
-        with open(yaml_path_i, 'r', encoding="utf-8") as yaml_i:
+        with open(yaml_path_i, "r", encoding="utf-8") as yaml_i:
             doc = yaml.safe_load(yaml_i)
         for key in data_list_i:
             doc[key] = data_list_i[key]
-        with open(yaml_path_i, 'w', encoding="utf-8") as yaml_i:
+        with open(yaml_path_i, "w", encoding="utf-8") as yaml_i:
             yaml.safe_dump(doc, yaml_i, default_flow_style=False)
 
         # Replace path to yaml with path to job yaml in submit script
-        with open(submit_path_i, 'r', encoding="utf-8") as submit_i:
+        with open(submit_path_i, "r", encoding="utf-8") as submit_i:
             doc = submit_i.read()
-        doc = doc.replace(os.path.abspath(base_dir),os.path.abspath(job_dir))
-        with open(submit_path_i, 'w', encoding="utf-8") as submit_i:
+        doc = doc.replace(os.path.abspath(base_dir), os.path.abspath(job_dir))
+        with open(submit_path_i, "w", encoding="utf-8") as submit_i:
             submit_i.write(doc)
 
+
 def submit_jobs(
-        configs,
-        base_dir="./",
-        submit_path="submit.sh",
-        out_path="jobs.txt",
-        aliases=None,
-        dry_run=False
-    ):
+    configs,
+    base_dir="./",
+    submit_path="submit.sh",
+    out_path="jobs.txt",
+    aliases=None,
+    dry_run=False,
+):
     """
     Parameters
     ----------
@@ -115,17 +113,19 @@ def submit_jobs(
     for data_list_i in data_list:
 
         # Get job directory name
-        job_dir = os.path.join(base_dir, get_config_str(data_list_i,aliases=aliases))
+        job_dir = os.path.join(base_dir, get_config_str(data_list_i, aliases=aliases))
         data_list_i["outdir"] = os.path.abspath(job_dir)
 
         # Get submit script name
-        submit_path_i = os.path.join(job_dir,os.path.basename(submit_path))
+        submit_path_i = os.path.join(job_dir, os.path.basename(submit_path))
 
         # Submit job to SLURM
-        command  = 'echo \''+str(counter)
-        command += ' sbatch '+os.path.abspath(submit_path_i)
-        command += '\' >> '+out_path+'; '
+        command = "echo '" + str(counter)
+        command += " sbatch " + os.path.abspath(submit_path_i)
+        command += "' >> " + out_path + "; "
         if not dry_run:
-            command = command+'sbatch '+os.path.abspath(submit_path_i)+' >> '+out_path
+            command = (
+                command + "sbatch " + os.path.abspath(submit_path_i) + " >> " + out_path
+            )
         subprocess.run(command, shell=True, check=True, text=True)
         counter += 1
