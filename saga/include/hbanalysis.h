b3f4c4bcc254fdc3aec8279550542a5fceaff140
Matthew F. McEneaney <mfmceneaney@gmail.com>
2026-02-04 17:06:05 -0500

    fix: Updated debug messages.

--- saga/include/hbanalysis.h
+++ saga/include/hbanalysis.h
@@ -997,7 +997,7 @@ void getKinBinnedHB(
 
         // Create signal region dataset for sideband subtraction
         if (use_sb_subtraction) {
-            LOG_DEBUG(Form("[%s]: Creating sideband dataset...", method_name.c_str()));
+            LOG_DEBUG(Form("[%s]: Creating signal dataset...", method_name.c_str()));
             data::createDataset(
                 binframe_sg,
                 ws_sg, //NOTE: Use separate sideband workspace for dataset, variable, and pdf name uniqueness.
@@ -1221,7 +1221,6 @@ void getKinBinnedHB(
         if (use_sb_subtraction) {
             LOG_DEBUG(Form("[%s]: Applying sideband subtraction...", method_name.c_str()));
             int k2 = 1 + 2 * (binvars.size() + depolvars.size() + rawasymvars.size());
-            LOG_DEBUG(Form("[%s]: k2 = %d AND asymfit_result_sb.size() = %d", method_name.c_str(), k2, asymfit_result_sb.size()));
             epsilon = eps_bg_pdf;
             epsilon_err = eps_bg_pdf_err;
             for (int idx=0; idx<nparams; idx++) {
